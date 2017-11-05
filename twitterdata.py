8602c72a7c85c392eaaac914cf46ac1bf2e0e5e1
Matheus Marotzke <matheusmarotzke@Matheuss-MacBook-Pro.local>
2017-11-05 17:21:33 -0200

    error handling request frequency exceding allowed

--- twitterdata.py
+++ twitterdata.py
@@ -2,7 +2,6 @@
 
 from twitter import *
 import re
-import csv
 import datetime
 import time
 import boto3
@@ -28,18 +27,21 @@ def send_to_s3(file_name):
     os.remove(file_name)
 
 while True:
-    count = 0
-    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
-    auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"])
-    stream = TwitterStream(auth = auth, secure = True)
-    tweet_iter = stream.statuses.filter(track = search_term)
-    file_name = "twitter-data-{0}.txt".format(st)
-    with open(file_name, "w") as f:
-        for tweet in tweet_iter:
-            f.write(str(tweet))
-            f.write("\n")
-            count += 1
-            if count == 100:
-                break
-    t1 = threading.Thread(target=send_to_s3, args=(file_name,))
-    t1.start()
+    try:
+        count = 0
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
+        auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"])
+        stream = TwitterStream(auth = auth, secure = True)
+        tweet_iter = stream.statuses.filter(track = search_term)
+        file_name = "twitter-data-{0}.txt".format(st)
+        with open(file_name, "w") as f:
+            for tweet in tweet_iter:
+                f.write(str(tweet))
+                f.write("\n")
+                count += 1
+                if count == 10000:
+                    break
+        t1 = threading.Thread(target=send_to_s3, args=(file_name,))
+        t1.start()
+    except:
+        time.sleep(1000)
