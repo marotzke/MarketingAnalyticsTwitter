e02b0f1fc3b2f1ff49d3a0d8665ac731f927dc09
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-11-15 20:20:02 -0200

    [done] basic api for proccessed text

--- spark_consumer.py
+++ spark_consumer.py
@@ -1,11 +1,9 @@
 import nltk.sentiment.vader as vader
-import logging
+import requests
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-
-
 
 
 def mapping_func(line):
@@ -20,19 +18,32 @@ def mapping_func(line):
         return (0, 1, 0, 1)
 
 
+def send_rdd(rdd):
+    tmp = rdd.take(1)[0]
+    requests.post(url, json={
+        'good': tmp[0],
+        'neutral': tmp[1],
+        'bad': tmp[2],
+        'count': tmp[3]
+    })
+
+
 if __name__ == "__main__":
     sc = SparkContext(appName="PythonStreamingKafkaWordCount")
     ssc = StreamingContext(sc, 15)
     sia = vader.SentimentIntensityAnalyzer()
     zkQuorum = 'localhost:2181'
     topic = 'trump'
-    logger = logging.getLogger('py4j')
+    url = 'http://127.0.0.1:5000/'
+
 
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
     lines = kvs.map(lambda x: x[1])
 
     counts = lines.map(mapping_func) \
                   .reduce(lambda x, y: [xx + yy for xx, yy in zip(x, y)])
+
+    counts.foreachRDD(lambda x: send_rdd(x))  # counts is a dstream (stream of rdds)
     counts.pprint()
 
     ssc.start()
